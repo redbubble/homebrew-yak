c23ba24a94e1e31b2ad73f453cd46e104be905f8
toothbrush <paul@denknerd.org>
2018-05-04 13:44:12 +1000

    Brew formula update for yak version v1.2.0

--- Formula/yak.rb
+++ Formula/yak.rb
@@ -1,9 +1,9 @@
 class Yak < Formula
   desc "A tool to log in to AWS through Okta"
   homepage "https://github.com/redbubble/yak"
-  url "https://github.com/redbubble/yak/releases/download/v1.1.3/yak_1.1.3_darwin_amd64.tar.gz"
-  version "1.1.3"
-  sha256 "a933656ebe9764f065d5fc04bcfa503d12bcf58b918be6bced8ddc79605c4a0d"
+  url "https://github.com/redbubble/yak/releases/download/v1.2.0/yak_1.2.0_darwin_amd64.tar.gz"
+  version "1.2.0"
+  sha256 "43c7de90a536c63af4d0401afde2b471a142d55b5aa8083d405cc20eba56e77c"
 
   def install
     bin.install "yak"
